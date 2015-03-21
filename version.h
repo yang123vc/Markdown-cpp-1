6a0bc0bfef68aed73886774793d59d02c218ea00
Jonas Zinn <Jonas.Zinn@uni-konstanz.de>
2015-03-21 03:40:32 +0100

    small changes

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "26";
+	static const char DATE[] = "27";
 	static const char MONTH[] = "02";
 	static const char YEAR[] = "2015";
 	static const char UBUNTU_VERSION_STYLE[] =  "15.02";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
-	static const long BUILD  = 5;
-	static const long REVISION  = 26;
+	static const long BUILD  = 9;
+	static const long REVISION  = 57;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 16;
-	#define RC_FILEVERSION 1,0,5,26
-	#define RC_FILEVERSION_STRING "1, 0, 5, 26\0"
-	static const char FULLVERSION_STRING [] = "1.0.5.26";
+	static const long BUILDS_COUNT  = 21;
+	#define RC_FILEVERSION 1,0,9,57
+	#define RC_FILEVERSION_STRING "1, 0, 9, 57\0"
+	static const char FULLVERSION_STRING [] = "1.0.9.57";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 5;
+	static const long BUILD_HISTORY  = 9;
 	
 
 }
